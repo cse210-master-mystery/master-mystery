3326991631d5bc2b353302f46b4c87a3131f2d58
ayah825 <156377548+ayah825@users.noreply.github.com>
2026-02-02 22:57:54 -0800

    attempt at responsive design for screen ratio

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,18 +2,24 @@ import menuPng from "../assets/images/menu.png";
 
 export default function Home() {
   return (
-    <div className="home">
-      <div className="center">
-        <h1 className="title">Matter Mystery</h1>
+    <div className="screen-wrapper">
+      <div className="scalable-box">
 
-        <button className="btnStart" onClick={() => console.log("start")}>
-          Start
-        </button>
-      </div>
+        <div className="home">
+          <div className="center">
+            <h1 className="title">Matter Mystery</h1>
 
-      <button className="menuBtn" onClick={() => console.log("menu")} aria-label="Open menu">
-        <img src={menuPng} alt="" draggable={false} />
-      </button>
+            <button className="btnStart" onClick={() => console.log("start")}>
+              Start
+            </button>
+          </div>
+
+          <button className="menuBtn" onClick={() => console.log("menu")} aria-label="Open menu">
+            <img src={menuPng} alt="" draggable={false} />
+          </button>
+        </div>
+
+    </div>
     </div>
   );
 }
