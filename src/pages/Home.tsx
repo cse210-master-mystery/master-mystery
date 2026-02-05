73b91849bd70a30c6f26ff8539295e3e266a9814
ayah825 <156377548+ayah825@users.noreply.github.com>
2026-02-04 22:22:47 -0800

    properly scalled the screen ratio

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,26 +4,23 @@ import menuPng from "../assets/images/menu.png";
 export default function Home() {
   return (
     <div className="wrapper">
-    <div className="game-scale">
-      <div className="background">
+      <div className="game-scale">
 
-      <div className="home">
-        <div className="center">
-          <h1 className="title">Matter Mystery</h1>
+        <div className="home">
+          <div className="center">
+            <h1 className="title">Matter Mystery</h1>
 
-          <button className="btnStart" onClick={() => console.log("start")}>
-            Start
-          </button> 
+            <button className="btnStart" onClick={() => console.log("start")}>
+              Start
+            </button> 
+          </div>
+
+            <button className="menuBtn" onClick={() => console.log("menu")} aria-label="Open menu">
+            <img src={menuPng} alt="" draggable={false} />
+          </button>
         </div>
 
-          <button className="menuBtn" onClick={() => console.log("menu")} aria-label="Open menu">
-          <img src={menuPng} alt="" draggable={false} />
-        </button>
       </div>
-
-      </div>
-
-    </div>
     </div>
   );
 }
