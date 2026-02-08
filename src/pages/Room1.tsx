1878c0c63a2af9d5042be43f5e5035e7192b1e8a
ayah825 <156377548+ayah825@users.noreply.github.com>
2026-02-07 18:03:11 -0800

    formating: used prettier

--- src/pages/Room1.tsx
+++ src/pages/Room1.tsx
@@ -4,30 +4,17 @@ import lever2img from "../assets/images/lever2.png";
 import bookimg from "../assets/images/book.png";
 
 export default function Room1() {
-        const navigate = useNavigate();
-        // make event handler for each button to react to click 
-        return (
-            <div className="wrapper">
-                <div className="game-scale">
-                    <div className="room1bkg">
-                    <img
-                        src={lever1img}
-                        className="btnlever1"
-                        onClick={() => navigate("/master-mystery")}
-                    />
-                    <img
-                        src={lever2img}
-                        className="btnlever2"
-                        onClick={() => navigate("/master-mystery")}
-                    />
-                    <img
-                        src={bookimg}
-                        className="btnbook"
-                        onClick={() => navigate("/master-mystery")}
-                    />
-                </div>
-            </div>
+  const navigate = useNavigate();
+  // make event handler for each button to react to click
+  return (
+    <div className="wrapper">
+      <div className="game-scale">
+        <div className="room1bkg">
+          <img src={lever1img} className="btnlever1" onClick={() => navigate("/master-mystery")} />
+          <img src={lever2img} className="btnlever2" onClick={() => navigate("/master-mystery")} />
+          <img src={bookimg} className="btnbook" onClick={() => navigate("/master-mystery")} />
         </div>
-        );
-  }
-  
+      </div>
+    </div>
+  );
+}
