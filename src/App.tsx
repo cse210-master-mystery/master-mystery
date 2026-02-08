54618644fccf492ed26574a32d28ffa7548f2ec4
ayah825 <156377548+ayah825@users.noreply.github.com>
2026-02-07 17:40:00 -0800

    added lever1 and room1 images

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 
 import Home from "./pages/Home";
 import Room1 from "./pages/Room1";
--- src/pages/Room1.tsx
+++ src/pages/Room1.tsx
@@ -1,10 +1,20 @@
-export default function Home() {
-    return (
-      <div className="wrapper">
-        <div className="game-scale">
-          h
+import { useNavigate } from "react-router-dom";
+import lever1img from "../assets/images/lever1.png";
+
+export default function Room1() {
+        const navigate = useNavigate();
+        return (
+            <div className="wrapper">
+                <div className="game-scale">
+                    <div className="room1bkg">
+                    <img
+                        src={lever1img}
+                        className="btnlever1"
+                        onClick={() => navigate("/master-mystery")}
+                    />
+                </div>
+            </div>
         </div>
-      </div>
-    );
+        );
   }
   
