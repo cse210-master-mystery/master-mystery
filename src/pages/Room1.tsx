6823960aee236e815cad33af4b0107100bcc601b
ayah825 <156377548+ayah825@users.noreply.github.com>
2026-02-07 17:58:06 -0800

    added buttons to room1

--- src/pages/Room1.tsx
+++ src/pages/Room1.tsx
@@ -1,8 +1,11 @@
 import { useNavigate } from "react-router-dom";
 import lever1img from "../assets/images/lever1.png";
+import lever2img from "../assets/images/lever2.png";
+import bookimg from "../assets/images/book.png";
 
 export default function Room1() {
         const navigate = useNavigate();
+        // make event handler for each button to react to click 
         return (
             <div className="wrapper">
                 <div className="game-scale">
@@ -10,6 +13,16 @@ export default function Room1() {
                     <img
                         src={lever1img}
                         className="btnlever1"
+                        onClick={() => navigate("/master-mystery")}
+                    />
+                    <img
+                        src={lever2img}
+                        className="btnlever2"
+                        onClick={() => navigate("/master-mystery")}
+                    />
+                    <img
+                        src={bookimg}
+                        className="btnbook"
                         onClick={() => navigate("/master-mystery")}
                     />
                 </div>
