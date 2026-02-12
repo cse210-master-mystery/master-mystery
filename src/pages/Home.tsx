cc97c5edb100314e24f9a074cadce81957cae008
Asakuraken <yangchenyu0101@gmail.com>
2026-02-12 15:45:00 -0800

    change menu style; add clock

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,23 @@
-import menuPng from "../assets/images/menu.png";
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const navigate = useNavigate();
+  const [now, setNow] = useState(new Date());
+
+  useEffect(() => {
+    const id = setInterval(() => setNow(new Date()), 1000);
+    return () => clearInterval(id);
+  }, []);
+
+  const timeText = now.toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
   return (
     <div className="wrapper">
       <div className="game-scale">
+        <div className="game-clock">{timeText}</div>
         <div className="home">
           <div className="center">
             <h1 className="title">Matter Mystery</h1>
@@ -15,8 +27,8 @@ export default function Home() {
             </button>
           </div>
 
-          <button className="menuBtn" onClick={() => console.log("menu")} aria-label="Open menu">
-            <img src={menuPng} alt="" draggable={false} />
+          <button className="btnMenu" onClick={() => console.log("menu")} aria-label="Open menu">
+            Menu
           </button>
         </div>
       </div>
