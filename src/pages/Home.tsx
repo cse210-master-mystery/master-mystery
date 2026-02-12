0589dd36cb72f42d3b1d1db1c363d8ff5674c966
Tony Nguyen <tonynguyeenn@gmail.com>
2026-02-12 00:11:43 -0800

    Adding testing for button components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import menuPng from "../assets/images/menu.png";
 import { useNavigate } from "react-router-dom";
+import HintButton from "../components/buttons/HintButton";
 
 export default function Home() {
   const navigate = useNavigate();
@@ -18,6 +19,7 @@ export default function Home() {
           <button className="menuBtn" onClick={() => console.log("menu")} aria-label="Open menu">
             <img src={menuPng} alt="" draggable={false} />
           </button>
+          <HintButton hint="Welcome to Matter Mystery! Click 'Start' to begin your adventure. Explore the rooms, find clues, and solve puzzles to uncover the mystery. Good luck!" />
         </div>
       </div>
     </div>
