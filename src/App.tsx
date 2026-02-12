da86fd3cd2d9fd9001e1e6946f0c912eee6f997b
imnishanth <nishanth.ramesh1@gmail.com>
2026-02-12 13:38:26 -0800

    adding FailPage component for time-out scenario

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Room1 from "./pages/Room1";
 import EndPage from "./pages/EndPage";
+import FailPage from "./pages/FailPage";
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
         <Route path="/master-mystery" element={<Home />} />
         <Route path="/Room1" element={<Room1 />} />
         <Route path="/EndPage" element={<EndPage />} />
+        <Route path="/FailPage" element={<FailPage />} />
       </Routes>
     </BrowserRouter>
   );
