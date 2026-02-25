eec31ad8a4c18dc29242830e83a0da4a210b1fec
Tony <93945730+TonyNgyen@users.noreply.github.com>
2026-02-25 15:15:20 -0800

    Making changes in according to PR

--- src/pages/Storyline/Storyline.tsx
+++ src/pages/Storyline/Storyline.tsx
@@ -6,11 +6,11 @@ import Button from "../../components/buttons/Button";
 const storySlides = [
   {
     text: "During a school tour of a secret underground containment vault, a mad scientist demonstrates an experiment that stores matter at extreme energy levels.",
-    effect: "fade",
+    effect: "",
   },
   {
     text: "The system overloads, shrinking you to particle size and trapping you inside the experiment chambers.",
-    effect: "vibrate",
+    effect: "danger",
   },
   {
     text: "Solve the puzzles to escape! You have 15 minutes to find your way out, or else…",
@@ -33,13 +33,13 @@ export default function Storyline() {
   };
 
   return (
-    <div className={`story-page-wrapper`}>
-      <div key={index} className={`story-card-size story-card-${currentSlide.effect}`}>
-        <p className="story-text">{currentSlide.text}</p>
+    <div className="wrapper">
+      <div className={`story-card-size story-card-${currentSlide.effect}`}>
+        <p key={index} className="story-text">
+          {currentSlide.text}
+        </p>
 
-        <Button fontSize="clamp(16px, calc(4 * var(--unit)), 200px)" onClick={handleNext}>
-          {index === storySlides.length - 1 ? "READY" : "NEXT"}
-        </Button>
+        <Button onClick={handleNext}>{index === storySlides.length - 1 ? "READY" : "NEXT"}</Button>
       </div>
     </div>
   );
